f574a5aa628b3517f923104cb61d9fdc773e57d4
Steven <12050358+Sibz@users.noreply.github.com>
2024-05-18 08:38:09 +0000

    feat!: remove ref

--- README.md
+++ README.md
@@ -4,8 +4,26 @@ Trait that adds a bound function enabling to restrict a number to a range.
 
 Automatically implemented for anything that implements std trait `Ord`.
 
+## V1 Breaking change
+
+Removed the requirement to operate only on references.
+
+Previously this worked:
+```
+let number = 19;
+let upper = 60;
+let low = 10;
+
+assert_eq!(number.bound(&low, &upper), &number);
+```
+Now the bound method doesn't take self as ref so the equivilant is now:
+`assert_eq!(number.as_ref().bound(&low, &upper), &number);`
+However unless passing references is preferred, copy should suffice:
+`assert_eq!(number.bound(low, upper), number);`
+Only use references if type is larger than a reference.
+
 ## Usage
-`bound(&self, lower: &Self, upper: &Self) -> &Self`
+`bound(self, lower: Self, upper: Self) -> Self`
 ```rust
 
 use num_bound::Bound;
@@ -20,9 +38,9 @@ fn bound_test()
     let out_upper = 600;
     let in_bounds = 300;
     
-    assert_eq!(out_lower.bound(&l, &u), &lower);
-    assert_eq!(out_upper.bound(&l, &u), &upper);
-    assert_eq!(in_bounds.bound(&l, &u), &in_bounds);
+    assert_eq!(out_lower.bound(l, u), lower);
+    assert_eq!(out_upper.bound(l, u), upper);
+    assert_eq!(in_bounds.bound(l, u), in_bounds);
 }
 
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,73 @@
-#[doc = include_str!("../README.md")]
+/**
+ * 
+This module defines a trait `Bound` that provides a method `bound` for bounding a value between a lower and upper limit.
+
+*/
 pub trait Bound: Ord
 {
-    fn bound<'a>(&'a self, lower: &'a Self, upper: &'a Self) -> &'a Self
-    {
-        lower.max(self.min(upper))
-    }
+    /// Bound a value between a lower and upper limit
+    /// 
+    /// ### Arguments
+    /// 
+    /// * `lower` - The lower limit
+    /// * `upper` - The upper limit
+    /// 
+    /// ### Returns
+    /// 
+    /// The value bounded between the lower and upper limit
+    /// 
+    /// ### Example
+    /// 
+    /// ```
+    /// use num_bound::Bound;
+    ///     
+    /// assert_eq!(1.bound(0, 2), 1);
+    /// assert_eq!(0.bound(0, 2), 0);
+    /// assert_eq!(2.bound(0, 2), 2);
+    /// assert_eq!((-1).bound(0, 2), 0);
+    /// assert_eq!(3.bound(0, 2), 2);
+    /// ```
+    fn bound(self, lower: Self, upper: Self) -> Self;    
 }
+
 
 impl<T: Ord> Bound for T
 {
+    #[inline(always)]
+    fn bound(self, lower: T, upper: T) -> T
+    {
+        if self < lower
+        {
+            lower
+        }
+        else if self > upper
+        {
+            upper
+        }
+        else
+        {
+            self
+        }
+    }
+}
+
+
+#[test]
+fn test_bound_ref()
+{
+    assert_eq!((&1).bound(&0, &2), &1);
+    assert_eq!((&0).bound(&0, &2), &0);
+    assert_eq!((&2).bound(&0, &2), &2);
+    assert_eq!((&-1).bound(&0, &2), &0);
+    assert_eq!((&3).bound(&0, &2), &2);
+}
+
+#[test]
+fn test_bound()
+{
+    assert_eq!(1.bound(0, 2), 1);
+    assert_eq!(0.bound(0, 2), 0);
+    assert_eq!(2.bound(0, 2), 2);
+    assert_eq!((-1).bound(0, 2), 0);
+    assert_eq!(3.bound(0, 2), 2);
 }
